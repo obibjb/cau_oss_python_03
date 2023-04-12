ed8c2657a53a3454c7736243a6fdced7c36035c2
obibjb <128777885+obibjb@users.noreply.github.com>
2023-04-12 19:50:30 +0900

    Create prac1.py

---
+++ prac1.py
@@ -0,0 +1,8 @@
+hw_score=input("Homework:")
+att_score=inut("Attendance:")
+mid_score=input("Mid-term exam:")
+fin_score=input("Final exam:")
+
+total_score=hw_score+att_score+mid_score+fin_score
+
+orint("Total score:",total score)
